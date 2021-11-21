ced76b1859ff57e59e702ebb102dd4321a70f132
Semm6 <sem.dewilde6@gmail.com>
2021-11-21 19:39:08 +0100

    Initial Commit

---
+++ Swift-Auth0-poc/Swift_Auth0_pocApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Auth0_pocApp.swift
+//  Swift-Auth0-poc
+//
+//  Created by Sem de Wilde on 21/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Auth0_pocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Swift-Auth0-pocUITests/Swift_Auth0_pocUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Swift_Auth0_pocUITestsLaunchTests.swift
+//  Swift-Auth0-pocUITests
+//
+//  Created by Sem de Wilde on 21/11/2021.
+//
+
+import XCTest
+
+class Swift_Auth0_pocUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
